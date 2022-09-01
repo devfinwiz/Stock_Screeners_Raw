6afa3c2c563bc50a71b9f65aa5db1413192d3879
DEV_FINWIZ <devjuneja43@gmail.com>
2022-09-01 18:06:53 +0530

    Second metric added to determine fair value

--- Scipts/Valuation.py
+++ Scipts/Valuation.py
@@ -11,43 +11,47 @@ val_as_per_bv=[]
 val_as_per_pts=[]
 latest_close=[]
 
-with open("Auto generated Dataset\FinancialsBunch.csv",'r') as mf:
-    data=csv.DictReader(mf)
+#-----------------------------------------------------------------------------------------
+#Computes fair value for all tickers as per their book value and as per their annual sales
 
-    for row in data:
-        try:
-            bv=float(row['Book Value'])
-            pts=float(row['PToSales'])
-            ltp=float(row['Close'])
-            tickers.append(row['Ticker'])
-            latest_close.append(row['Close'])
+def valuation_computer():
+    with open("Auto generated Dataset\FinancialsBunch.csv",'r') as mf:
+        data=csv.DictReader(mf)
 
-            if(pts>0):
-                fairvaluebv=1.5*bv
-                val_as_per_bv.append(fairvaluebv)
-            else:
-                fairvaluebv=bv
-                val_as_per_bv.append(fairvaluebv)
+        for row in data:
+            try:
+                bv=float(row['Book Value'])
+                pts=float(row['PToSales'])
+                ltp=float(row['Close'])
+                tickers.append(row['Ticker'])
+                latest_close.append(row['Close'])
+
+                if(pts>0):
+                    fairvaluebv=1.5*bv
+                    val_as_per_bv.append(fairvaluebv)
+                else:
+                    fairvaluebv=bv
+                    val_as_per_bv.append(fairvaluebv)
+                
+                fairvaluepts=(float(row['Revenue'])/float(row['Shares Outstanding']))
+                holder=((2-pts)/2)*100
+                holder2=(float(row['Close'])*float(holder))/100
+                fairvaluepts2=float(row['Close'])+holder2
+
+                finalfairvaluepts=(fairvaluepts+fairvaluepts2)/2
+
+                val_as_per_pts.append(finalfairvaluepts)
+                
             
-            fairvaluepts=(float(row['Revenue'])/float(row['Shares Outstanding']))
-            holder=((2-pts)/2)*100
-            holder2=(float(row['Close'])*float(holder))/100
-            fairvaluepts2=float(row['Close'])+holder2
+            except Exception as e:
+                print(row['Ticker'])
+                print(traceback.format_exc())
 
-            finalfairvaluepts=(fairvaluepts+fairvaluepts2)/2
+    list_clubber=[tickers,val_as_per_bv,val_as_per_pts,latest_close]
+    export_data_complete=zip_longest(*list_clubber,fillvalue='')
 
-            val_as_per_pts.append(finalfairvaluepts)
-            
-        
-        except Exception as e:
-            print(row['Ticker'])
-            print(traceback.format_exc())
-
-list_clubber=[tickers,val_as_per_bv,val_as_per_pts,latest_close]
-export_data_complete=zip_longest(*list_clubber,fillvalue='')
-
-with open("Auto generated Dataset\Valuations.csv",'w',encoding="ISO-8859-1",newline="") as myfile:
-    wr=csv.writer(myfile)
-    wr.writerow(("Ticker","Valuation As Per Book Value","Valuation As Per Sales","LTP"))
-    wr.writerows(export_data_complete)
-    print("Execution Success!")
+    with open("Auto generated Dataset\Valuations.csv",'w',encoding="ISO-8859-1",newline="") as myfile:
+        wr=csv.writer(myfile)
+        wr.writerow(("Ticker","Valuation As Per Book Value","Valuation As Per Sales","LTP"))
+        wr.writerows(export_data_complete)
+        print("Execution Success!")
