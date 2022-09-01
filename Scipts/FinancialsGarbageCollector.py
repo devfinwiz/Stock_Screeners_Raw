5c0fe46c06385852bc8d5d2d7cd885eb7b4d1387
DEV_FINWIZ <devjuneja43@gmail.com>
2022-09-01 18:13:51 +0530

    Minor change in dummy value

--- Scipts/FinancialsGarbageCollector.py
+++ Scipts/FinancialsGarbageCollector.py
@@ -3,36 +3,41 @@
 
 import csv
 
-f=open("Auto generated Dataset\FinancialsBunch.csv","w",newline="") 
+#------------------------------------------------------------------------------------
+#Performs data preprocessing by replacing the incomplete data with dummy value 22222
 
-with open('Auto generated Dataset\Financials.csv',"r+") as mf:
-    data=csv.DictReader(mf)
-    wr=csv.writer(f)
-    wr.writerow(("Ticker","Book Value","EVToEBITDA","PToBV","MarketCap","PToSales","Close","Shares Outstanding","Revenue"))
+def fgarbage_collector():
 
-    for row in data:
-       
-        if(row['PToBV']=="" and row['EVToEBITDA']=="" and row['PToSales']==""):
-           wr.writerow((row['Ticker'],row['Book Value'],"22222","22222",row['MarketCap'],"22222",row['Close'],row['Shares Outstanding'],row['Revenue']))
+   f=open("Auto generated Dataset\FinancialsBunch.csv","w",newline="") 
 
-        elif(row['EVToEBITDA']=="" and row['PToBV']==""):
-            wr.writerow((row['Ticker'],row['Book Value'],"22222","22222",row['MarketCap'],row['PToSales'],row['Close'],row['Shares Outstanding'],row['Revenue']))
+   with open('Auto generated Dataset\Financials.csv',"r+") as mf:
+      data=csv.DictReader(mf)
+      wr=csv.writer(f)
+      wr.writerow(("Ticker","Book Value","EVToEBITDA","PToBV","MarketCap","PToSales","Close","Shares Outstanding","Revenue"))
 
-        elif(row['EVToEBITDA']=="" and row['PToSales']==""):
-            wr.writerow((row['Ticker'],row['Book Value'],"22222",row['PToBV'],row['MarketCap'],"22222",row['Close'],row['Shares Outstanding'],row['Revenue']))
+      for row in data:
+         
+         if(row['PToBV']=="" and row['EVToEBITDA']=="" and row['PToSales']==""):
+            wr.writerow((row['Ticker'],row['Book Value'],"22222","22222",row['MarketCap'],"22222",row['Close'],row['Shares Outstanding'],row['Revenue']))
 
-        elif(row['PToBV']=="" and row['PToSales']==""):
-            wr.writerow((row['Ticker'],row['Book Value'],row['EVToEBITDA'],"22222",row['MarketCap'],"22222",row['Close'],row['Shares Outstanding'],row['Revenue']))
+         elif(row['EVToEBITDA']=="" and row['PToBV']==""):
+               wr.writerow((row['Ticker'],row['Book Value'],"22222","22222",row['MarketCap'],row['PToSales'],row['Close'],row['Shares Outstanding'],row['Revenue']))
 
-        elif(row['PToBV']==""):
-           wr.writerow((row['Ticker'],row['Book Value'],row['EVToEBITDA'],"22222",row['MarketCap'],row['PToSales'],row['Close'],row['Shares Outstanding'],row['Revenue']))
+         elif(row['EVToEBITDA']=="" and row['PToSales']==""):
+               wr.writerow((row['Ticker'],row['Book Value'],"22222",row['PToBV'],row['MarketCap'],"22222",row['Close'],row['Shares Outstanding'],row['Revenue']))
 
-        elif(row['EVToEBITDA']==""):
-           wr.writerow((row['Ticker'],row['Book Value'],"22222",row['PToBV'],row['MarketCap'],row['PToSales'],row['Close'],row['Shares Outstanding'],row['Revenue']))
+         elif(row['PToBV']=="" and row['PToSales']==""):
+               wr.writerow((row['Ticker'],row['Book Value'],row['EVToEBITDA'],"22222",row['MarketCap'],"22222",row['Close'],row['Shares Outstanding'],row['Revenue']))
 
-        elif(row['PToSales']==""):
-           wr.writerow((row['Ticker'],row['Book Value'],row['EVToEBITDA'],row['PToBV'],row['MarketCap'],"22222",row['Close'],row['Shares Outstanding'],row['Revenue']))
+         elif(row['PToBV']==""):
+            wr.writerow((row['Ticker'],row['Book Value'],row['EVToEBITDA'],"22222",row['MarketCap'],row['PToSales'],row['Close'],row['Shares Outstanding'],row['Revenue']))
 
-        else:
-           wr.writerow((row['Ticker'],row['Book Value'],row['EVToEBITDA'],row['PToBV'],row['MarketCap'],row['PToSales'],row['Close'],row['Shares Outstanding'],row['Revenue']))
+         elif(row['EVToEBITDA']==""):
+            wr.writerow((row['Ticker'],row['Book Value'],"22222",row['PToBV'],row['MarketCap'],row['PToSales'],row['Close'],row['Shares Outstanding'],row['Revenue']))
+
+         elif(row['PToSales']==""):
+            wr.writerow((row['Ticker'],row['Book Value'],row['EVToEBITDA'],row['PToBV'],row['MarketCap'],"22222",row['Close'],row['Shares Outstanding'],row['Revenue']))
+
+         else:
+            wr.writerow((row['Ticker'],row['Book Value'],row['EVToEBITDA'],row['PToBV'],row['MarketCap'],row['PToSales'],row['Close'],row['Shares Outstanding'],row['Revenue']))
 
