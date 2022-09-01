331a5eaf864aeca244d1c9108df222e588a229b2
DEV_FINWIZ <devjuneja43@gmail.com>
2022-09-01 18:14:49 +0530

    Demerged P/S filter

--- Scipts/Fundamental_Screener3.py
+++ Scipts/Fundamental_Screener3.py
@@ -12,33 +12,37 @@ tickers_MCap=list()
 tickers_bookval=list()
 tickers_close=list()
 
-with open("Auto generated Dataset\FinancialsBunch.csv",'r') as mf:
-    data=csv.DictReader(mf)
+#------------------------------------------------------
+#Filters stocks with attractive P/S of below 1.25 value
 
-    for row in data:
+def pts_filter():
+    with open("Auto generated Dataset\FinancialsBunch.csv",'r') as mf:
+        data=csv.DictReader(mf)
 
-        try:
-            hold2=float(row['PToSales'])
-            hold2=round(hold2,3)
-            #print(hold2<1)
+        for row in data:
 
-        except Exception as e:
-            print(row['Ticker'])
-            print(traceback.format_exc())
+            try:
+                hold2=float(row['PToSales'])
+                hold2=round(hold2,3)
+                #print(hold2<1)
 
-        if(hold2<1.25):
-                tickers_name.append(row['Ticker'])
-                tickers_MCap.append(row['MarketCap'])
-                tickers_PTS.append(row['PToSales'])
-                tickers_close.append(row['Close'])
-                tickers_bookval.append(row['Book Value'])
+            except Exception as e:
+                print(row['Ticker'])
+                print(traceback.format_exc())
+
+            if(hold2<1.25):
+                    tickers_name.append(row['Ticker'])
+                    tickers_MCap.append(row['MarketCap'])
+                    tickers_PTS.append(row['PToSales'])
+                    tickers_close.append(row['Close'])
+                    tickers_bookval.append(row['Book Value'])
 
 
-list_clubber=[tickers_name,tickers_MCap,tickers_PTS,tickers_close,tickers_bookval]
-export_data_complete=zip_longest(*list_clubber,fillvalue='')
+    list_clubber=[tickers_name,tickers_MCap,tickers_PTS,tickers_close,tickers_bookval]
+    export_data_complete=zip_longest(*list_clubber,fillvalue='')
 
-with open("Auto generated Dataset\MCapPTS.csv",'w',encoding="ISO-8859-1",newline="") as myfile:
-    wr=csv.writer(myfile)
-    wr.writerow(("Ticker","Market Cap","P/S","LTP","Book Value"))
-    wr.writerows(export_data_complete)
-    print("Execution Success!")
+    with open("Auto generated Dataset\MCapPTS.csv",'w',encoding="ISO-8859-1",newline="") as myfile:
+        wr=csv.writer(myfile)
+        wr.writerow(("Ticker","Market Cap","P/S","LTP","Book Value"))
+        wr.writerows(export_data_complete)
+        print("Execution Success!")
